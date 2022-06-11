6b58fcd27b4158b86521c6cd155bdd039160a264
Kim Bauters <kim.bauters@smarthound.uk>
2022-06-11 11:05:22 +0100

    add exceptions to the import list for the package

--- chai_persistence/__init__.py
+++ chai_persistence/__init__.py
@@ -1,5 +1,10 @@
-from chai_persistence.main import Homes
+from chai_persistence.main import Homes, DeviceType, SetpointMode
+from chai_data_sources.exceptions import NetatmoError, EfergyError
 
 __all__ = [
     "Homes",
+    "DeviceType",
+    "SetpointMode",
+    "NetatmoError",
+    "EfergyError",
 ]
