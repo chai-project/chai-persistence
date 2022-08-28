5c6401decb86d44377356b189fc5119fe424ca15
Kim Bauters <kim.bauters@bristol.ac.uk>
2022-08-28 09:46:59 +0100

    add an 'on error resume next' strategy

--- chai_persistence/home_persistence_thread.py
+++ chai_persistence/home_persistence_thread.py
@@ -54,60 +54,63 @@ class HomePersistenceThread(threading.Thread):
             base = datetime(time.year, time.month, time.day, time.hour, tz=self._tz)
             current_start = base.add(minutes=completed * self._interval.value)
             current_end = base.add(minutes=(completed + 1) * self._interval.value)
-            current_middle = base.add(minutes=(completed + 0.5) * self._interval.value)
-            if not bootstrapped:
-                # wait until the middle of an interval, either the current one or the next one
-                middle = current_middle if time < current_middle else current_middle.add(minutes=self._interval.value)
-                bootstrapped = True
+            current_mid = base.add(minutes=(completed + 0.5) * self._interval.value)
+            try:
+                if not bootstrapped:
+                    # wait until the middle of an interval, either the current one or the next one
+                    middle = current_mid if time < current_mid else current_mid.add(minutes=self._interval.value)
+                    bootstrapped = True
 
-                debug("range:  %s – %s", current_start.isoformat(), current_end.isoformat())
-                debug("waiting until %s before continuing to align the logging", middle.isoformat())
-                until(middle.int_timestamp)
-                debug("bootstrap complete")
-                continue
+                    debug("range:  %s – %s", current_start.isoformat(), current_end.isoformat())
+                    debug("waiting until %s before continuing to align the logging", middle.isoformat())
+                    until(middle.int_timestamp)
+                    debug("bootstrap complete")
+                    continue
 
-            debug("performing data polling")
+                debug("performing data polling")
 
-            session: Session
-            with db_session(self._st_session) as session:
-                home: Home = session.query(Home).filter_by(id=self._home_db_id).one()
-                debug("got the home instance")
-                try:
-                    value = self._relay.thermostat_temperature
-                    # noinspection PyTypeChecker
-                    # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
-                    entry = NetatmoReading(room_id=1, relay=home.relay,
-                                           start=current_start, end=current_end, reading=value)
-                    debug("prepared thermostat reading")
-                    session.add(entry)
-                    debug("stored thermostat reading")
-                except NetatmoError:
-                    pass
-                try:
-                    value = self._relay.valve_temperature
-                    # noinspection PyTypeChecker
-                    # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
-                    entry = NetatmoReading(room_id=2, relay=home.relay,
-                                           start=current_start, end=current_end, reading=value)
-                    debug("prepared valve reading")
-                    session.add(entry)
-                    debug("stored valve reading")
-                except NetatmoError:
-                    pass
-                try:
-                    value = self._relay.valve_percentage
-                    # noinspection PyTypeChecker
-                    # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
-                    entry = NetatmoReading(room_id=3, relay=home.relay,
-                                           start=current_start, end=current_end, reading=value)
-                    debug("prepared valve status reading")
-                    session.add(entry)
-                    debug("stored valve status reading")
-                except NetatmoError:
-                    pass
-                debug("storing in DB")
-            debug(f"next polling at %s", current_middle.add(minutes=self._interval.value).isoformat())
-            until(current_middle.add(minutes=self._interval.value).int_timestamp)
+                session: Session
+                with db_session(self._st_session) as session:
+                    home: Home = session.query(Home).filter_by(id=self._home_db_id).one()
+                    debug("got the home instance")
+                    try:
+                        value = self._relay.thermostat_temperature
+                        # noinspection PyTypeChecker
+                        # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
+                        entry = NetatmoReading(room_id=1, relay=home.relay,
+                                               start=current_start, end=current_end, reading=value)
+                        debug("prepared thermostat reading")
+                        session.add(entry)
+                        debug("stored thermostat reading")
+                    except NetatmoError:
+                        pass
+                    try:
+                        value = self._relay.valve_temperature
+                        # noinspection PyTypeChecker
+                        # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
+                        entry = NetatmoReading(room_id=2, relay=home.relay,
+                                               start=current_start, end=current_end, reading=value)
+                        debug("prepared valve reading")
+                        session.add(entry)
+                        debug("stored valve reading")
+                    except NetatmoError:
+                        pass
+                    try:
+                        value = self._relay.valve_percentage
+                        # noinspection PyTypeChecker
+                        # ignore the warnings; DateTime is a datetime.datetime (compatible) instance
+                        entry = NetatmoReading(room_id=3, relay=home.relay,
+                                               start=current_start, end=current_end, reading=value)
+                        debug("prepared valve status reading")
+                        session.add(entry)
+                        debug("stored valve status reading")
+                    except NetatmoError:
+                        pass
+                    debug("storing in DB")
+            except Exception as err:
+                debug(f"Encountered an unexpected and unhandled error: {err}\nSaving the thread by ignoring the error.")
+            debug(f"next polling at %s", current_mid.add(minutes=self._interval.value).isoformat())
+            until(current_mid.add(minutes=self._interval.value).int_timestamp)
 
     def stop(self):
         """ Cancel/stop the execution of this thread. """
