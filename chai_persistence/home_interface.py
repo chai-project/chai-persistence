817a387b2e1da48de2db34e3966106bb99486d8e
Kim Bauters <kim.bauters@bristol.ac.uk>
2023-04-05 08:16:58 +0100

    update docs and reformat

--- chai_persistence/home_interface.py
+++ chai_persistence/home_interface.py
@@ -11,26 +11,35 @@ from chai_persistence.home_persistence_thread import HomePersistenceThread
 
 class HomeInterface:
     """
-    Provide a dummy interface to the Netatmo relay installed in a home.
+    Provide an interface to the Netatmo relay installed in a home, which itself starts the data collection thread.
     Unless stopped, the interface logs temperature according to the indicated interval.
     """
-    _relay: NetatmoClient
-    _thread: HomePersistenceThread
     _home_db_id: int
     _st_session: scoped_session
+    _relay: NetatmoClient
+    _thread: HomePersistenceThread
 
     def __init__(self, *, home_db_id: int, st_session: scoped_session,
                  netatmo_refresh_token: str, client_id: str, client_secret: str,
                  netatmo_target: Optional[str] = None,
                  interval: Minutes = Minutes.MIN_5):
+
+        # set the variables related to this interface
         self._home_db_id = home_db_id
         self._st_session = st_session
-        self._relay = NetatmoClient(client_id=client_id, client_secret=client_secret,
-                                    refresh_token=netatmo_refresh_token,
-                                    **({"target": netatmo_target} if netatmo_target else {}))
+        self._relay = NetatmoClient(
+            client_id=client_id,
+            client_secret=client_secret,
+            refresh_token=netatmo_refresh_token,
+            **({"target": netatmo_target} if netatmo_target else {})
+        )
+
         # start a thread to log the temperature for both valve and thermostat, as well as the valve status
         self._thread = HomePersistenceThread(
-            relay=self._relay, home_db_id=home_db_id, st_session=st_session, interval=interval
+            relay=self._relay,
+            home_db_id=home_db_id,
+            st_session=st_session,
+            interval=interval
         )
         self._thread.start()
 
